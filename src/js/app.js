6b4de9a1e7a24f7c8f4a70f69effab8b5fba8e5e
Muhammad Soban <msoban54@gmail.com>
2021-04-08 19:58:11 +0500

    sticky color changes

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,12 @@ $(function () {
   $(window).scroll(function () {
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
+      $(".navbar .menu li a").addClass("sticky");
+      $(".navbar .logo a span").addClass("sticky");
     } else {
       $(".navbar").removeClass("sticky");
+      $(".navbar .menu li a").removeClass("sticky");
+      $(".navbar .logo a span").removeClass("sticky");
     }
   });
 });
