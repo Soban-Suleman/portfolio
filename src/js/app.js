2c54778955dd809bf572ddc03c6b4ea89301b9a3
Muhammad Soban <msoban54@gmail.com>
2021-04-08 22:10:53 +0500

    hamburger menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,18 @@
 $(function () {
   $(window).scroll(function () {
     if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
-      $(".navbar .menu li a").addClass("sticky");
-      $("#portfolio span,#portfolio").addClass("sticky");
+      $(".navbar,.navbar .menu li a,#portfolio span,#portfolio").addClass(
+        "sticky"
+      );
     } else {
-      $(".navbar").removeClass("sticky");
-      $(".navbar .menu li a").removeClass("sticky");
-      $("#portfolio span,#portfolio").removeClass("sticky");
+      $("#portfolio span,#portfolio,.navbar .menu li a,.navbar").removeClass(
+        "sticky"
+      );
     }
+  });
+
+  $(".menu-btn").click(function () {
+    console.log("here");
+    $(".navbar .menu").toggleClass("active");
   });
 });
